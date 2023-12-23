097dfbfdbfb664a0941ebd9fb7d5ec8a3128b3ef
Yoseph1994 <joshrde2002@example.com>
2023-12-23 12:28:11 +0300

    Separated to diffrent files

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import Steps from "./Steps";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,44 +7,9 @@ const messages = [
   "Invest your new income 🤑",
 ];
 function App() {
-  const [handleClose, setHandleClose] = useState(true);
-  const [handleCloseText, setHandleCloseText] = useState("Hide");
-  const [step, setStep] = useState(0);
-  function onSetChangeClose() {
-    setHandleClose(!handleClose);
-    setHandleCloseText(handleClose ? "Show" : "Hide");
-  }
-
-  function onNext() {
-    setStep(step < messages.length ? step + 1 : 0);
-  }
-
-  function onPrevious() {
-    setStep(step >= 0 ? step - 1 : 0);
-  }
   return (
     <>
-      <button onClick={onSetChangeClose} className="close">
-        {handleCloseText}
-      </button>
-      {handleClose && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={`step-1 ${step === 0 ? "active" : ""}`}>1</div>
-            <div className={`step-1 ${step === 1 ? "active" : ""}`}>2</div>
-            <div className={`step-1 ${step === 2 ? "active" : ""}`}>3</div>
-          </div>
-          <p className="message">{messages[step]}</p>
-          <div className="buttons">
-            <button className="button previous" onClick={onPrevious}>
-              Previous
-            </button>
-            <button className="next" onClick={onNext}>
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+      <Steps messages={messages} />
     </>
   );
 }
---
+++ steps/src/Steps.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Steps({ messages }) {
+  const [handleClose, setHandleClose] = useState(true);
+  const [handleCloseText, setHandleCloseText] = useState("Hide");
+  const [step, setStep] = useState(0);
+  function onSetChangeClose() {
+    setHandleClose(!handleClose);
+    setHandleCloseText(handleClose ? "Show" : "Hide");
+  }
+
+  function onNext() {
+    setStep(step < messages.length ? step + 1 : 0);
+  }
+
+  function onPrevious() {
+    setStep(step >= 0 ? step - 1 : 0);
+  }
+  return (
+    <>
+      <button onClick={onSetChangeClose} className="close">
+        {handleCloseText}
+      </button>
+      {handleClose && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`step-1 ${step === 0 ? "active" : ""}`}>1</div>
+            <div className={`step-1 ${step === 1 ? "active" : ""}`}>2</div>
+            <div className={`step-1 ${step === 2 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">{messages[step]}</p>
+          <div className="buttons">
+            <button className="button previous" onClick={onPrevious}>
+              Previous
+            </button>
+            <button className="next" onClick={onNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Steps;
