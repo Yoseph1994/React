0b1816fae82887cea83e3a1f0ef4d549c05a0da8
Yoseph1994 <joshrde2002@example.com>
2023-12-23 11:33:18 +0300

    Made Toggler effect

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import "./App.css";
-
 function App() {
+  const [handleClose, setHandleClose] = useState(true);
+  const [handleCloseText, setHandleCloseText] = useState("Hide");
+  function onSetChangeClose() {
+    setHandleClose(!handleClose);
+    setHandleCloseText(handleClose ? "Show" : "Hide");
+  }
   return (
     <>
-      <button>X</button>
-      <div className="steps">
-        <div className="numbers">
-          <div className="step-1">1</div>
-          <div className="step-2">2</div>
-          <div className="step-3">3</div>
+      <button onClick={onSetChangeClose}>{handleCloseText}</button>
+      {handleClose && (
+        <div className="steps">
+          <div className="numbers">
+            <div className="step-1 active">1</div>
+            <div className="step-2">2</div>
+            <div className="step-3">3</div>
+          </div>
+          <p className="message"></p>
+          <div className="buttons">
+            <button className="previous">Previous</button>
+            <button className="next">Next</button>
+          </div>
         </div>
-        <p className="message"></p>
-        <div className="buttons">
-          <button className="previous">Previous</button>
-          <button className="next">Next</button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
